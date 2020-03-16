5e06c5b727e48ec300afc16648913cf481e9a2fc
iamgeorge <george.eyoyibo@gmail.com>
2020-03-16 18:10:47 +0200

    add about & contact page

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>This is the about page</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim assumenda
+        accusantium quod aperiam saepe, similique dolores nam reprehenderit sed
+        a sint voluptas, id earum quas? Doloribus doloremque quisquam cum ullam!
+      </p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ src/pages/contact.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>This is the contact page</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde id
+        tenetur assumenda doloribus quibusdam asperiores perferendis sit ratione
+        autem, error impedit possimus facere animi reprehenderit excepturi
+        doloremque qui, eum fugiat.
+      </p>
+    </div>
+  )
+}
+
+export default ContactPage
