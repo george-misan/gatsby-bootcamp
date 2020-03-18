b04ae04cf18aa18c9043a7189ade044c36c1050f
iamgeorge <george.eyoyibo@gmail.com>
2020-03-19 00:37:41 +0200

    rendering contentful posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,6 +7,43 @@ import blogStyles from "./blog.module.scss"
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
     query {
+      allContentfulBlogPost(sort: { fields: publishedDate, order: DESC }) {
+        edges {
+          node {
+            title
+            slug
+            publishedDate(formatString: "MMMM Do, YYYY")
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <h1>Blog</h1>
+      <ol className={blogStyles.posts}>
+        {data.allContentfulBlogPost.edges.map(edge => {
+          return (
+            <li key={parseInt(Math.random() * 100)} className={blogStyles.post}>
+              <Link to={`/blog/${edge.node.slug}`}>
+                <h2>{edge.node.title}</h2>
+                <p>{edge.node.publishedDate}</p>
+              </Link>
+            </li>
+          )
+        })}
+      </ol>
+    </Layout>
+  )
+}
+
+export default BlogPage
+
+// Mark down query
+/* const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
       allMarkdownRemark {
         edges {
           node {
@@ -41,4 +78,4 @@ const BlogPage = () => {
   )
 }
 
-export default BlogPage
+export default BlogPage */
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -3,7 +3,7 @@ import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 
-export const query = graphql`
+/* export const query = graphql`
   query($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       frontmatter {
@@ -13,16 +13,22 @@ export const query = graphql`
       html
     }
   }
+` */
+
+export const query = graphql`
+  query($slug: String!) {
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      publishedDate(formatString: "MMMM Do, YYYY")
+    }
+  }
 `
 
 const Blog = props => {
   return (
     <Layout>
-      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-      <p>{props.data.markdownRemark.frontmatter.date}</p>
-      <div
-        dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
-      ></div>
+      <h1>{props.data.contentfulBlogPost.title}</h1>
+      <p>{props.data.contentfulBlogPost.publishedDate}</p>
     </Layout>
   )
 }
